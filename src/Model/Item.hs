02f54fda63c81e7a8e22cef11131efcfa3c3dd46
Taras Serdyuk <taras.serdyuk@gmail.com>
2012-11-15 18:26:05 +0200

    item price

--- src/Model/Item.hs
+++ src/Model/Item.hs
@@ -14,6 +14,7 @@ import Text.Blaze
 data Item = Item {
     getName :: Name,
     getStatus :: Status,
+    getPrice :: Price,
     getDesc :: Description,
     getItemId :: ItemId }
     deriving (Eq, Ord, Typeable)
@@ -22,6 +23,7 @@ data Status = Free | Booked deriving (Eq, Ord, Show, Typeable)
 
 newtype ItemId = Id Integer deriving (Eq, Ord, Enum, ToMarkup, Typeable)
 newtype Name = Name Text deriving (Eq, Ord, ToMarkup, Typeable)
+newtype Price = Price Int deriving (Eq, Ord, ToMarkup, Typeable)
 newtype Description = Desc Text deriving (Eq, Ord, ToMarkup)
 
 
@@ -29,7 +31,8 @@ instance Indexable Item where
     empty = ixSet [
         ixFun1 getItemId,
         ixFun1 getName,
-        ixFun1 getStatus ] where
+        ixFun1 getStatus,
+        ixFun1 getPrice ] where
         ixFun1 f = ixFun $ return . f
 
 
@@ -41,4 +44,5 @@ $(deriveSafeCopy 0 'base ''Item)
 $(deriveSafeCopy 0 'base ''ItemId)
 $(deriveSafeCopy 0 'base ''Name)
 $(deriveSafeCopy 0 'base ''Status)
+$(deriveSafeCopy 0 'base ''Price)
 $(deriveSafeCopy 0 'base ''Description)
--- src/Model/Sample.hs
+++ src/Model/Sample.hs
@@ -8,7 +8,7 @@ import Model.Item
 
 initItems :: IdSet Item ItemId
 initItems = foldr put (empty (Id 0)) [
-    Item (Name "Item A") Free (Desc "Some description."),
-    Item (Name "Item B") Free (Desc "Some description."),
-    Item (Name "Item C") Free (Desc "Some description."),
-    Item (Name "Item D") Booked (Desc "Some description.")]
+    Item (Name "Item A") Free (Price 40) (Desc "Some description."),
+    Item (Name "Item B") Free (Price 20) (Desc "Some description."),
+    Item (Name "Item C") Free (Price 30) (Desc "Some description."),
+    Item (Name "Item D") Booked (Price 50) (Desc "Some description.")]
